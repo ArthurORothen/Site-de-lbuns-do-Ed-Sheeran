7d9c319eb396bb756596e59a5e5fde645532c17e
ArthurORothen <arthurrothen2@gmail.com>
2026-03-19 17:17:42 -0300

    imagens e criação do app

---
+++ app.js
@@ -0,0 +1,22 @@
+let albumAtivo = {
+    imgFundo: document.querySelector('body').style.backgroundImage = "url('imgs/img_X.svg')",
+    imgAlbum: document.querySelector('.imgAlbum'),
+    listaMusicas: [
+        "One",
+        "I'm a Mess",
+        "Sing",
+        "Don't",
+        "Nina",
+        "Photograph"
+    ]
+}
+
+
+
+function proximo(){
+
+}
+
+function anterior(){
+
+}
